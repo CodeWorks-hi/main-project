a393f88455d0f5d93fc7058bf475fa9511726f16
choeyeonu2411 <choeyeonu2411@gmail.com>
2025-04-03 18:29:51 +0900

    Update C_A_analytics_sale_domestic.py

--- modules/C_A_analytics_sale_domestic.py
+++ modules/C_A_analytics_sale_domestic.py
@@ -7,11 +7,6 @@
 import pandas as pd
 import streamlit as st
 
-from .C_A_analytics_sale_domestic_goal import domestic_goal_ui
-from .C_A_analytics_sale_domestic_growth import domestic_growth_ui
-from .C_A_analytics_sale_domestic_performance import domestic_performance_ui
-from .C_A_analytics_sale_domestic_region import domestic_region_ui
-
 def domestic_ui():
 
     # 탭 구성
@@ -19,11 +14,5 @@ def domestic_ui():
           "국내실적","지역별 비교", "목표 달성률", "성장률 분석"
     ])
 
-    with tab1:
-        domestic_performance_ui()
-    with tab2:
-        domestic_region_ui()
-    with tab3:
-        domestic_goal_ui()
-    with tab4:
-        domestic_growth_ui()
+
+
