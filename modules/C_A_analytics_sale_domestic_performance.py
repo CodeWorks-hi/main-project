7b32f582898cd6ead0982dacf729cf6e15d7dd9e
mybear1109 <mybear8211@gmail.com>
2025-04-03 18:20:23 +0900

    .

--- modules/C_A_analytics_sale_domestic_performance.py
+++ modules/C_A_analytics_sale_domestic_performance.py
@@ -6,8 +6,17 @@
 
 
 import streamlit as st
+import pandas as pd
+
+
+car_list_path = "data/hyundae_car_list.csv"
+inventory_path = "data/inventory_data.csv"
+customer_path = "data/customer_data.csv"
+
+df_inv = pd.read_csv(inventory_path)
+df_list = pd.read_csv(car_list_path)
+df_customer = pd.read_csv(customer_path)
 
 def domestic_performance_ui():
-    st.subheader("판매·수출 관리")
-    st.write("국내 판매 실적을 분석하는 페이지입니다.")
-    st.write("차종/지역별 실적 분석")
+    st.subheader(" 국내 실적 요약")
+    st.write("고객의 전체 구매 실적 및 기본 통계 정보를 표시합니다.")
